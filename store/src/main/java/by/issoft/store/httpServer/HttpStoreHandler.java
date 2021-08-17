1ce74e88db0189e63240b41a91ecad041ebf31ff
MarinaGotka <marina.gotka@gmail.com>
2021-08-17 18:46:14 +0300

    removed response field

--- store/src/main/java/by/issoft/store/httpServer/HttpStoreHandler.java
+++ store/src/main/java/by/issoft/store/httpServer/HttpStoreHandler.java
@@ -9,12 +9,10 @@ import java.io.OutputStream;
 
 public class HttpStoreHandler implements HttpHandler {
 
-    private static String response;
-
     protected void handle(HttpExchange httpExchange, Object responseObject) throws IOException {
 
         ObjectMapper objectMapper = new ObjectMapper();
-        response = objectMapper.writeValueAsString(responseObject);
+        String response = objectMapper.writeValueAsString(responseObject);
 
         httpExchange.sendResponseHeaders(200, response.length());
         OutputStream os = httpExchange.getResponseBody();
